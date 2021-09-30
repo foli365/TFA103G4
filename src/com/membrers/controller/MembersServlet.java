b888f9f7567512cc8a8aeca71b63448dddfe2184
Yves Pan <foli365@gmail.com>
2021-09-30 16:12:02 +0800

    no message

--- src/com/membrers/controller/MembersServlet.java
+++ src/com/membrers/controller/MembersServlet.java
@@ -8,12 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MembersServlet extends HttpServlet {
-	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doPost(req, res);
 	}
-	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		req.setCharacterEncoding("UTF-8");
