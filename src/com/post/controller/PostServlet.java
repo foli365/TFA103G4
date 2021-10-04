37d9bddd0ec9d2ab7c74b00ee34e1daaf852a925
yvespan <foli365@gmail.com>
2021-10-04 21:44:28 +0800

    no message

--- src/com/post/controller/PostServlet.java
+++ src/com/post/controller/PostServlet.java
@@ -1,17 +1,12 @@
 package com.post.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.LinkedList;
-import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.emp.model.EmpService;
 import com.post.model.PostService;
 import com.post.model.PostVO;
 
